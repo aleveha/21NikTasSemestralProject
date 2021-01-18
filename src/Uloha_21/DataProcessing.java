5b7026b9342f0bc0491b4f38747bb140bb793a30
aleveha <nikita.tashilin@gmail.com>
2021-01-18 21:38:50 +0100

    edit rotation

--- src/Uloha_21/DataProcessing.java
+++ src/Uloha_21/DataProcessing.java
@@ -11,7 +11,7 @@ public class DataProcessing {
 
         for (int i = 0; i < 4; i++) {
             if (checkMatrix(rotatingMatrix, comparedMatrix)) {
-                inputData.setDegree(degree == 0 ? degree : 360 - degree);
+                inputData.setDegree(/*degree == 0 ? degree : 360 -*/ degree);
             }
             degree += 90;
             rotateMatrix(rotatingMatrix);
@@ -25,7 +25,7 @@ public class DataProcessing {
      */
     public static boolean checkMatrix(int[][] rotatingMatrix, int[][] comparedMatrix) {
         boolean rotation = true;
-        for(int i = 0; i < rotatingMatrix.length; i++) {
+        for (int i = 0; i < rotatingMatrix.length; i++) {
             for (int k = 0; k < rotatingMatrix.length; k++) {
                 if (rotatingMatrix[i][k] != comparedMatrix[i][k]) {
                     rotation = false;
@@ -45,11 +45,39 @@ public class DataProcessing {
     public static void rotateMatrix(int[][] matrix) {
         int row = matrix.length;
         //first find the transpose of the matrix
+//        for (int i = 0; i < row; i++) {
+//            for (int j = i + 1; j < row; j++) {
+//                int temp = matrix[i][j];
+//                matrix[i][j] = matrix[j][i];
+//                matrix[j][i] = temp;
+//            }
+//        }
+
+        /*
+         * 1 2 3
+         * 4 5 6
+         * 7 8 9
+         * */
+
+        /*
+         * 9 6 3
+         * 8 5 2
+         * 7 4 1
+         * */
+
+        /*
+         * 3 6 9
+         * 2 5 8
+         * 1 4 7
+         * */
+
         for (int i = 0; i < row; i++) {
-            for (int j = i; j < row; j++) {
+            for (int j = 0; j < row; j++) {
+                if (j == row - 1 - i) break;
+
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                matrix[i][j] = matrix[row - 1 - j][row - 1 - i];
+                matrix[row - 1 - j][row - 1 - i] = temp;
             }
         }
 
